e1a171e6fc83c749b284de17a58f33db4715a9f4
John R Perry <john@j-26.com>
2018-02-24 14:47:25 -0700

    Speed optimization
    Optimized the code to only load the necessary characters when the font library is loaded.

--- jquery.fontselect.js
+++ jquery.fontselect.js
@@ -390,10 +390,11 @@
       
       Fontselect.prototype.addFontLink = function(font){
       
+	var compressedString = font.replace(/(.)(?=.*\1)/g, "");
         var link = this.options.api + font;
       
         if ($("link[href*='" + font + "']").length === 0){
-			$('link:last').after('<link href="' + link + '" rel="stylesheet" type="text/css">');
+			$('link:last').after('<link href="' + link + '&text=' + compressedString + '" rel="stylesheet" type="text/css">');
 		}
       };
     
